dd6c41b8dc8d1a9db6829fb932a675d1e1c8c1cf
Vinicius Damascena <85317029+ViniciusDamascenaz@users.noreply.github.com>
2025-01-07 08:51:22 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webbrowser as web
 import time as tm
 from datetime import datetime, time
 
-numero_telefone = '1_4508505068'
+numero_telefone = "1_4508505068"
 hora_envio = time(hour=20, minute=30)  # Este é o horário de envio da mensagem
 
 while True:
@@ -11,7 +11,7 @@ while True:
     hora_atual = agora.time().replace(second=0, microsecond=0)  # Hora atual sem segundos
 
     if hora_atual == hora_envio:
-        web.open(f'https://web.whatsapp.com/send?phone={numero_telefone}')
+        web.open("https://web.whatsapp.com/send?phone="+numero_telefone)
         tm.sleep(25)  # Espera o WhatsApp Web carregar
         gui.write('BOA NOITE, FAMILIA!!')
         gui.press('enter')
